23d0bad3dad00ed5cff68248d86cdb7bd3e8a39b
hist0613 <hist0134@naver.com>
2020-04-04 22:16:48 +0900

    Modify relative-sort-array.py in leetcode

--- leetcode/relative-sort-array.py
+++ leetcode/relative-sort-array.py
@@ -1,8 +1,6 @@
-from collections import defaultdict
-
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        count = defaultdict(int)
+        count = [0] * 1001
         for num in arr1:
             count[num] += 1
         
@@ -10,9 +8,9 @@ class Solution:
         for num in arr2:
             for _ in range(count[num]):
                 ret.append(num)
-            count.pop(num)
+            count[num] = 0
                     
-        for num in sorted(count.keys()):
+        for num in range(1001):
             for _ in range(count[num]):
                 ret.append(num)
                 
